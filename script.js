042665f65682b40c6616a1fdf6404cd146365963
Gjoshiee <144885932+Gjoshiee@users.noreply.github.com>
2025-10-08 12:52:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showAboutAlert() {
+    alert("Thank you for visiting my portfolio! I love web development.");
+}
+
+function showContactAlert() {
+    alert("You can reach me at example@example.com");
+}
